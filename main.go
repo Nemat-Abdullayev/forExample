680063466ba5879745e85c303451c74123ba55bf
nemat.abdullayev <nemat.abdullayev5555@gmail.com>
2019-06-27 15:58:05 +0400

    we're doing great

--- main.go
+++ main.go
@@ -2,6 +2,23 @@ package main
 
 import "fmt"
 
-func main(){
-	fmt.Println("Salam necesen?")
+func main() {
+	fmt.Println("Hello MAN?")
+	foo()
+	fmt.Println("this is me!")
+
+	for i := 2; i < 100; i++ {
+		if i%2 == 0 {
+			fmt.Println(i)
+		}
+
+	}
+	bar()
+}
+func foo() {
+	fmt.Println("I'am in foo")
+}
+
+func bar()  {
+	fmt.Println("and the we exited")
 }
